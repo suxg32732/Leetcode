41989a832e6e763335a5cd09fac92ee9713683e8
suxg32732 <853032732@qq.com>
2017-12-14 18:11:51 +0800

    2017-12-14-c1

--- DeleteDuplicateMusic/src/Main.java
+++ DeleteDuplicateMusic/src/Main.java
@@ -6,8 +6,11 @@ public class Main {
     public static String resPath = "d:"+ File.separator + "MyMusic" + File.separator + "Music";
     public static String desPath = "d:"+ File.separator + "MyMusic" + File.separator + "test";
 
+    public static String testPath = "d:"+ File.separator + "test";
+
     public static void main(String[] args) {
-        DeleteDuplicatedMusicFiles.getDeleteDuplicatedMusicFiles(resPath);
+//        DeleteDuplicatedMusicFiles.getDeleteDuplicatedMusicFiles(resPath);
+        DeleteDuplicatedMusicFiles.getDeleteDuplicatedMusicFiles(testPath);
     }
 
 

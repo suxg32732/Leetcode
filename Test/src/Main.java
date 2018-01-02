e5362c449af22602380f311c2d12ddb692a41e16
suxg32732 <853032732@qq.com>
2018-01-02 20:06:03 +0800

    2018-1-2c

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.HashSet;
+
 public class Main {
+
+    private static HashSet<Integer> val = new HashSet<Integer>();
+    private static int size = val.size();
+
     public static void main(String[] args) {
-        System.out.println(0xfff);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(getSize());
+            val.add(i);
+        }
+    }
+
+    public static int getSize() {
+        return val.size();
     }
 }

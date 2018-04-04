31579bfd691971b53fc04ae451218733c812d2e6
suxg32732 <853032732@qq.com>
2018-04-04 17:50:09 +0800

    2018.04.04-c1

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -8,47 +8,7 @@ public class Main {
 
 
     public static void main(String[] args)  {
-
-
         System.out.println(b);
-/*        Person p = new Person();
-        Class person = p.getClass();
-        try {
-            Object o =  person.newInstance();
-            Field[] fileds = person.getDeclaredFields();
-            for (Field filed : fileds) {
-                filed.setAccessible(true);
-                System.out.println(filed.get(p));
-            }
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }*/
-//        1521475200000L
-/*        System.out.println(281479653330760L);
-
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Long.MAX_VALUE);
-        System.out.println(Double.MAX_VALUE);
-        System.out.println((double)281479653330760L );
-//        System.out.println(10E13);
-//        System.out.println((double)1/3);
-
-
-        System.out.println((double) 336966472);*/
-//        System.out.println("aadsa".replace("aadsa", null));
-
-//        ArrayList<String> a = new ArrayList<String>();
-//        System.out.println(a.get(10));
-//        System.out.println(new Date(System.currentTimeMillis()));
-//        ArrayList<String> list = new ArrayList<>(300000);
-//        for (int i = 0; i < 30000; i++) {
-//            list.add("" + i);
-//        }
-//        System.out.println(new Date(System.currentTimeMillis()));
-//        System.out.println("Done");
-
     }
 
 }

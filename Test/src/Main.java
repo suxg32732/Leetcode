e058b0efc68a33d00aaed392baac2fdc51a2a908
suxg32732 <853032732@qq.com>
2018-04-09 18:05:30 +0800

    2018-04-09-C1

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Main {
 
 
@@ -8,7 +10,15 @@ public class Main {
 
 
     public static void main(String[] args)  {
-        System.out.println(b);
+        String[] a = {"1", "2", "3", "1"};
+        List<String> l1 = Arrays.asList(a);
+        List<String> l2 = Arrays.asList(a);
+        Set<String> s1 = new HashSet<String>(l1);
+        s1.addAll(l2);
+        System.out.println(l1);
+        System.out.println(s1);
+        System.out.println(new ArrayList<String>(s1));
+
     }
 
 }

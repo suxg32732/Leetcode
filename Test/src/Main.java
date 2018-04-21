d38780a6b66540611c2cf514c54bcee23806b878
suxg32732 <853032732@qq.com>
2018-04-21 17:57:22 +0800

    2018-4-21

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -10,14 +10,10 @@ public class Main {
 
 
     public static void main(String[] args)  {
-        String[] a = {"1", "2", "3", "1"};
-        List<String> l1 = Arrays.asList(a);
-        List<String> l2 = Arrays.asList(a);
-        Set<String> s1 = new HashSet<String>(l1);
-        s1.addAll(l2);
-        System.out.println(l1);
-        System.out.println(s1);
-        System.out.println(new ArrayList<String>(s1));
+        String a ="aa,aa, ";
+        int len = a.length();
+        if (a.endsWith(", "))
+            System.out.println(a.substring(0, len-1));
 
     }
 
